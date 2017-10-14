385f9be4dec3b72492536033c10ad8d58451ba12
Sayantika Banik <sayantikabanik122@gmail.com>
2017-10-14 21:29:58 +0530

    added linear search with javascript

---
+++ linearsearch.js
@@ -0,0 +1,27 @@
+ function linear()
+{
+    var n=parseInt(prompt("Enter the size of an array"));
+    var a=new Array(n);
+    var t=0;
+    for(var i=0; i<a.length; i++)
+    {
+        a[i]=parseInt(prompt("Enter array elements"));
+    }
+    var k=parseInt(prompt("Enter the key element to search: "));
+    for(var i=0; i<a.length; i++)
+    {
+        if(k==a[i])
+        {
+            t=1;
+            break;
+        }
+    }
+    if(t==1)
+    {
+        document.writeln("Element "+a[i]+ " Found at Position:"+i);
+    }
+    else
+    {
+        document.writeln("Element Not Found");
+    }
+}
