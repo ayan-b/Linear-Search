5e0b2693d503a23a686b7992c4a16823058a7bd5
Manav Mehra <manavmehra96@users.noreply.github.com>
2017-10-15 00:18:59 +0530

    Update linear search.cs

--- linear search.cs
+++ linear search.cs
@@ -10,14 +10,14 @@ namespace forgetCode
             Console.WriteLine("Enter number of elements you want to hold in the array ?");
             string s = Console.ReadLine();
             int x = Int32.Parse(s);
-            Console.WriteLine("-------------------------");
+            
             Console.WriteLine("\n Enter array elements \n");
             for (int i = 0; i < x; i++)
             {
                 string s1 = Console.ReadLine();
                 a[i] = Int32.Parse(s1);
             }
-            Console.WriteLine("-------------------------");
+            
             Console.WriteLine("Enter Search element\n");
             string s3 = Console.ReadLine();
             int x2 = Int32.Parse(s3);
@@ -25,7 +25,7 @@ namespace forgetCode
             {
                 if (a[i] == x2)
                 {
-                    Console.WriteLine("-------------------------");
+                   
                     Console.WriteLine("Search successful");
                     Console.WriteLine("Element {0} found at location {1}\n", x2, i + 1);
                     return;
