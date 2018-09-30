8b1ce7b30f4e10120f271379555e36cd11d3a890
mirkoperillo <mirko.perillo@gmail.com>
2018-09-30 17:45:21 +0200

    fix java code and refactor following project guideline

---
+++ java-linear-search/JavaLinearSearch.java
@@ -0,0 +1,20 @@
+// This is to search the element Manually in the list
+import java.util.*;
+public class JavaLinearSearch {
+
+    public static void main(String[] args) {
+        int myList[] = {1,2,3,8,9,12};
+        int key = 2;
+        String output = linearSearch(myList, key) ? "Successful Search" : "Unsuccessful Search";
+        System.out.println(output);
+    }
+
+    private static boolean linearSearch(int[] list, int key) {
+        for(int i = 0; i < list.length; i++) {
+            if(list[i] == key){
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- java-linear-search/README.md
+++ java-linear-search/README.md
@@ -1,2 +1,16 @@
 # java-linear-search
 *Example Linear Search in Java*
+
+## JavaLinearSearch.java
+
+```
+javac JavaLinearSearch.java && java JavaLinearSearch
+```
+
+
+## JavaLinearSearch_2.java
+
+```
+javac JavaLinearSearch_2.java && java JavaLinearSearch_2
+```
+
