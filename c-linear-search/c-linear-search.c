9e64e874562a26c49ad24f9bd54d88e29545b840
Aman Kumar Yadav <yadav.aman099@gmail.com>
2018-10-01 17:42:45 +0530

    Update c-linear-search.c

--- c-linear-search/c-linear-search.c
+++ c-linear-search/c-linear-search.c
@@ -3,13 +3,17 @@ int main()
 {
     int n,i,x,flag=0;                       
     int a[100100]; 
+    printf("Enter the size of array\n");
     scanf("%d",&n);            //how many numbers in which you want to search
+    printf("\nEnter the elements from which you want to search\n");
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);     //Taking input from user 
     } 
+    
+    printf("Enter the number to be searched\n");
     scanf("%d",&x);            // Number which we want to search
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++) //Looping over to the whole array!
     {
         if(a[i]==x)
             {
@@ -17,8 +21,8 @@ int main()
             }
     }    
     if(flag==1)
-        printf("Yes\n");      //If number is found then print yes.
+        printf("Found the Number\n");      //If number is found then print yes.
     else
-        printf("No\n");       // If number is not there then print no.
+        printf("The Number was NOT found\n");       // If number is not there then print no.
 return 0;
 }
