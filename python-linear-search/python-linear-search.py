f51b438450bde68ac939a091b32b598240de07bf
HaveYouMetNanu <31150168+HaveYouMetNanu@users.noreply.github.com>
2018-10-01 10:16:01 +0530

    Update python-linear-search.py

--- python-linear-search/python-linear-search.py
+++ python-linear-search/python-linear-search.py
@@ -1,11 +1,12 @@
-num_array = list() #Enter elements
+num_array = list()                                              #Enter elements
 num = raw_input("Enter how many elements you want:")
 print 'Enter numbers in array: '
 for i in range(int(num)):
-    n = raw_input("num :")
-    num_array.append(int(n))
+    n = raw_input("num :")                                    #taking elements as input
+    num_array.append(int(n))                                  #appending them to list
 element_to_find = raw_input("Element to find :")    
-for i in num_array: #searching through the list
+for i in num_array:                                           #searching through the list
     if i==element_to_find:
-        print ("Successful Search!")
+        print ("Successful Search and element found at ")
+        print (i)                                             #prints the index of element if found    
         break
