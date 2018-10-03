6e5f9af5199ff861ac8ce566ba6caa85286d9ae0
Aesophor <aesophor@mailfence.com>
2018-10-03 19:20:52 +0800

    Added Java, C, Bash and Python-3 linear search.

--- c-linear-search/c-linear-search-4.c
+++ c-linear-search/c-linear-search-4.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 
 int main(int argc, char* args[]) {
+    int data_size;
 
+    printf("Size of data: ");
+    scanf("%d", &data_size);
+
+    int data[data_size];
+
+    for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++) {
+        printf("Adding item (%d/%d): ", i+1, (int) (sizeof(data) / sizeof(data[0])));
+        scanf("%d", &data[i]);
+    }
+
+    for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++) {
+        printf("%d", data[i]);
+        if (i < sizeof(data) / sizeof(data[0]) - 1) {
+            printf(", ");
+        } else {
+            printf("\n");
+        }
+    }
+
+    printf("Perform sequential search for: ");
+    int target;
+    scanf("%d", &target);
+
+    for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++) {
+        if (target == data[i]) {
+            printf("Found item at index: %d\n", i);
+            return 0;
+        }
+    }
+    printf("Item not found.\n");
+
+    return 1;
 }
