88cff9180abd44fde6f6051bb3d566167adf7973
Anubhav Gupta <37771611+meetanubhav@users.noreply.github.com>
2018-10-03 13:57:33 +0530

    Linear search
    Linear search using python in few lines

---
+++ python3-linear-search/Linear_search_py3.py
@@ -0,0 +1,14 @@
+#Linear search using python in few lines:
+num=[5,3,6,8,6,22,5,89,84,4,2,1,6,9,5,3,59,1,33]
+seachnum = int(input("Enter the number you want to search"))
+
+def linear_search(num,searchnum):
+    counter=0;
+    for i in range (len(num)):
+        if(searchnum==num[i]):
+            print("Found!",searchnum,"at",i)
+            counter=counter+1
+    if(counter==0):
+        print(searchnum,'Not Found!')
+
+linear_search(num,searchnum)
