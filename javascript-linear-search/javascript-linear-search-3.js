f391de4f7a21cd6510b1da48923d0d20a0da850c
Josep <josep.arpi@gmail.com>
2018-10-03 20:57:37 +0200

    feat(#1): JS linear search - input

--- javascript-linear-search/javascript-linear-search-3.js
+++ javascript-linear-search/javascript-linear-search-3.js
@@ -1,3 +1,20 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question('Type the array elements, ie: 12345 ', (elements) => {
+  rl.question('Type the element to search ', (n) => {
+    const l = elements.length
+    const found = linearSearch(elements.split(''), n)
+    console.log(found > -1 ? `Element found at ${(l-found)}` : `No element found`)
+    rl.close();
+  })
+})
+
 const linearSearch = (arr, n) => {
-  return arr.length ? arr[0] === n || linearSearch(arr.slice(1), n) : false
+  return arr.length ? (arr[0] === n ? arr.length : linearSearch(arr.slice(1), n)) : -1
 }
+
