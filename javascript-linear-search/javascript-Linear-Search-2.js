569c71f8f2c0be70d381b149f60e0a868ea1150b
Mayank Kinger <kingermayank@gmail.com>
2018-10-06 20:58:01 +0530

    Added Java-LinearSearch-10

--- javascript-linear-search/javascript-Linear-Search-2.js
+++ javascript-linear-search/javascript-Linear-Search-2.js
@@ -1,6 +1,26 @@
-function linearSearch(arr, key){
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] === key) return i;
+function linear () {
+  let n = parseInt(prompt('Enter the size of an array'))
+  let a = []
+  for (let i = 0; i < n; i++) {
+    a[i] = parseInt(prompt('Current array:\t' + a + '\nEnter array elements'))
   }
-  return null;
+  let k = parseInt(prompt('Current array:\t' + a + '\nEnter the key element to search: '))
+  for (let i = 0; i < a.length; i++) {
+    if (k === a[i]) {
+      // document.body.innerText('');
+      document.writeln('Element ' + a[i] + ' Found at Position:' + i)
+      break
+    } else if (i === (a.length - 1) && k !== a[i]) {
+      // document.body.innerText('');
+      document.writeln('Element Not Found')
+    }
+  }
+}
+const nums = [1, 2, 3, 8, 9, 12]
+const target = 2
+for (const num of nums) {
+  if (num === target) {
+    console.log('Successful Search!')
+    break
+  }
 }
