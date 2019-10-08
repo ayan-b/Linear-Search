8efb98d3c121a02d2a5dcd56a0aff887e187e546
Agyeya Mishra <53916781+AgyeyaMishra@users.noreply.github.com>
2019-10-08 23:41:19 +0530

    Update linearSearch.c

--- C-linear-search/linearSearch.c
+++ C-linear-search/linearSearch.c
@@ -1,11 +1,13 @@
+//C program to implement linear search in an array
 #include <stdio.h>
-#include <conio.h>
+#include <conio.h>        /* Adding clrscr() and getch() */
 
 int main()
 {
   int array[100], search, c, n;
+  clrscr();
 
-  printf("Enter the number of elements in array\n");
+  printf("Enter the number of elements in array (max 100) : \n");
   scanf("%d", &n); /* Taking input for no of elements in array*/
 
   printf("Enter %d integer(s) :\n", n);
@@ -13,19 +15,20 @@ int main()
   for (c = 0; c < n; c++)
     scanf("%d", &array[c]); /* Taking input for values of array at each index*/
 
-  printf("Enter a number to search\n");
-  scanf("%d", &search); /* Taking input for element to search*/
+  printf("Enter the element to be searched :\n");
+  scanf("%d", &search); /* Taking input for element to be searched*/
 
   for (c = 0; c < n; c++)
   {
     if (array[c] == search)    /* If element is found */
     {
-      printf("%d is found at location %d.\n", search, c+1);
+      printf("%d is found at index %d and location %d.\n", search, c, c+1);
       break;
     }
   }
   if (c == n)
     printf("%d isn't found in the array.\n", search);
 
+  getch();
   return 0;
 }
